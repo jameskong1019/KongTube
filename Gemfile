66dc31af098c1c8976b84c607ef9d29a6f2c0533
James <jameskong1019@gmail.com>
2020-09-08 16:36:37 -0600

    add s3 storage

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem "certified"
 gem "kaminari"
 gem "jquery-rails", "~> 4.3", ">= 4.3.3"
 gem "rails-ujs", "~> 0.1.0"
+gem "fog-aws"
+gem "mini_magick"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,11 +1,14 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # # Choose what kind of storage to use for this uploader:
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
+  if Rails.env.production?
+      storage :fog
+  else
+      storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
