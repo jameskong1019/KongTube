c0042e72134e20d7c5aca5877833f8735260f620
James <jameskong1019@gmail.com>
2020-09-07 20:09:57 -0600

    fix for mobile

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,7 +8,7 @@ class VideosController < ApplicationController
   def index
     @videos = Video.all
     @videos = @videos.where("title like ?", "%#{params[:q]}%") if params[:q].present?
-    @videos = @videos.page(params[:page]).per(16)
+    @videos = @videos.page(params[:page]).per(4)
   end
 
   # GET /videos/1
@@ -34,7 +34,7 @@ class VideosController < ApplicationController
 
     respond_to do |format|
       if @video.save
-        format.html { redirect_to @video, notice: 'Video was successfully created.' }
+        format.html { redirect_to @video, notice: "Video was successfully created." }
         format.json { render :show, status: :created, location: @video }
       else
         format.html { render :new }
@@ -48,7 +48,7 @@ class VideosController < ApplicationController
   def update
     respond_to do |format|
       if @video.update(video_params)
-        format.html { redirect_to @video, notice: 'Video was successfully updated.' }
+        format.html { redirect_to @video, notice: "Video was successfully updated." }
         format.json { render :show, status: :ok, location: @video }
       else
         format.html { render :edit }
@@ -62,24 +62,25 @@ class VideosController < ApplicationController
   def destroy
     @video.destroy
     respond_to do |format|
-      format.html { redirect_to videos_url, notice: 'Video was successfully destroyed.' }
+      format.html { redirect_to videos_url, notice: "Video was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_video
-      @video = Video.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def video_params
-      params.require(:video).permit(:title, :description, :file, :image)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_video
+    @video = Video.find(params[:id])
+  end
 
-    # Check a user if it is a owner of the video
-    def check_owner
-      redirect_to root_path unless @video.user == current_user
-    end
+  # Only allow a list of trusted parameters through.
+  def video_params
+    params.require(:video).permit(:title, :description, :file, :image)
+  end
+
+  # Check a user if it is a owner of the video
+  def check_owner
+    redirect_to root_path unless @video.user == current_user
+  end
 end
